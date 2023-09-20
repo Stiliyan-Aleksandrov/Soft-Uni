d8a2601b41eda616964e9aa7187dbcf071ee3651
Stiliyan-Aleksandrov <alienskillzbg@gmail.com>
2023-09-20 23:31:56 +0300

    Update 09.login.js

--- JS/Basic Syntax, Conditional Statements and Loops - Exercise/09.login.js
+++ JS/Basic Syntax, Conditional Statements and Loops - Exercise/09.login.js
@@ -1,24 +1,24 @@
 function solve(input) {
     let username = input[0];
     let pass = ``;
-    let counter = 0;
-    let guess = input[1];
-    let index = 2;
     for (let i = username.length - 1; i >= 0; i--) {
         pass += username[i];
     }
+    let guess = input[1];
+    let index = 2;
+    let counter = 0;
     while (guess != pass) {
-        guess = input[index];
-        index++;
         counter++;
-        console.log(`Incorrect password. Try again.`);
         if (counter == 4) {
             console.log(`User ${username} blocked!`);
             return;
         }
-    }
-    console.log(`User ${username} logged in!`);
-}
-solve(['sunny', 'rainy', 'cloudy', 'not',
 
-    'sunny'])
+        console.log(`Incorrect password. Try again.`);
+
+        guess = input[index];
+        index++;
+    }
+    console.log(`User ${username} logged in.`);
+}
+solve(['sunny', 'rainy', 'cloudy', 'sunny', 'not', 'sunny'])
