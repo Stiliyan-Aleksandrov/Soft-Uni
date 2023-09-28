708d100a9f14fbd85742993195b6aa4fbca7ba46
Stiliyan-Aleksandrov <alienskillzbg@gmail.com>
2023-09-28 13:53:50 +0300

    Create 7. Lower or Upper.js

---
+++ JS/Data Types and Variables - Exercise/7. Lower or Upper.js
@@ -0,0 +1,7 @@
+function solve(char) {
+    let code = char.charCodeAt();
+    if (code >= 65 && code <= 90) {
+        console.log(`upper-case`);
+    } else { console.log(`lower-case`); }
+
+} solve('L')
