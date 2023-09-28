5ce7ce6cd7831e3519afc68acc4ebccabafd5fc9
Stiliyan-Aleksandrov <alienskillzbg@gmail.com>
2023-09-28 13:43:38 +0300

    04.fix

--- JS/Data Types and Variables - Exercise/04. Convert Meters to Kilometres.js
+++ JS/Data Types and Variables - Exercise/04. Convert Meters to Kilometres.js
@@ -1,5 +1,5 @@
-function solve(num) {
-    let meters=num;
-    let km=meters/100/10;
-    console.log(km.toFixed(2));
-}solve(1852)
+    function solve(num) {
+        let meters=num;
+        let km=meters/100/10;
+        console.log(km.toFixed(2));
+    }solve(1852)
---
+++ JS/Data Types and Variables - Exercise/05. Pounds to Dollars.js
@@ -0,0 +1,5 @@
+function solve(pound) {
+    let dollar=pound*1.31;
+    console.log(dollar.toFixed(3));
+    
+}solve(80)
