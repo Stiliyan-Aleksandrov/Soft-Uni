4d7c275f1965754430caf02e4eac6a22a30e35cc
Stiliyan-Aleksandrov <alienskillzbg@gmail.com>
2023-09-29 22:06:00 +0300

    rewrite

--- JS/Arrays/2. Day of Week.js
+++ JS/Arrays/2. Day of Week.js
@@ -1,16 +1,16 @@
-function solve(weekdays) {
-    if (weekdays > 7 || weekdays < 0) {
-        console.log(`"Invalid day!".`);
-    } else {
-        let days = [
-            `Monday`,
-            `Tuesdayt`,
-            `Wednesday`,
-            `Thursday`,
-            `Friday`,
-            `Saturday`,
-            `Sunday`
-        ];
-        console.log(days[weekdays - 1]);
-    }
-} solve(11)
+function solve(index) {
+    let days = [
+        `Monday`,
+        `Tuesdayt`,
+        `Wednesday`,
+        `Thursday`,
+        `Friday`,
+        `Saturday`,
+        `Sunday`
+    ];
+    let result=(days[index-1]);
+    if(result!=undefined){
+        console.log(result);
+    }else console.log(`Invalid day!`);
+}
+solve(5)
