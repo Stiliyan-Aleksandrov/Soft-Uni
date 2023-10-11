46c74fd328b7305c592b9aa89a73d74305ac68c3
Stiliyan-Aleksandrov <alienskillzbg@gmail.com>
2023-10-11 23:31:18 +0300

    04 Odd and even sum

---
+++ JS/15.Functions Excercise/03. Characters in Range.js
@@ -0,0 +1,18 @@
+function charInRange(char1, char2) {
+    let code1 = char1.charCodeAt(0);
+    let code2 = char2.charCodeAt(0);
+    let minCode = Math.min(code1, code2);
+    let maxCode = Math.max(code1, code2);
+    printResult(minCode, maxCode)
+
+    function printResult(minCode, maxCode) {
+        let result = ``;
+        for (let curCode = minCode + 1; curCode < maxCode; curCode++) {
+            let curChar = String.fromCharCode(curCode);
+            result += curChar + ` `;
+
+        }
+        console.log(result);
+    }
+
+}
